f9161fd22424970883e7722264d4965122c215af
Leonardo-ST <tavaresleonardo567@gmail.com>
2024-07-12 11:12:34 -0300

    o inicio

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer () {
+    return(<>
+
+<h1>Vai ficar o Footer</h1>
+
+    </>)
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar () {
+    return(<>
+
+<h1>Vai ficar a Navbar</h1>
+
+    </>)
+}
+
+export default Navbar
